6b8c7d49d7fc035a5b08196258ba5828bb08c04f
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-05-23 11:01:59 -0700

    WIP: Pair with Evan

--- src/Elm/Kernel/Test.js
+++ src/Elm/Kernel/Test.js
@@ -1,6 +1,8 @@
 /*
 
-import Dict exposing (empty, update)
+import Elm.Kernel.Utils exposing (Tuple0)
+import Elm.Kernel.List exposing (Nil, Cons)
+import Expect exposing (fail)
 
 */
 
@@ -9,5 +11,12 @@ import Dict exposing (empty, update)
 
 function _Test_runThunk(thunk)
 {
-  return "BAD;TODO";
+  try {
+    // Attempt to run the thunk as normal.
+    return thunk(__Utils_Tuple0);
+  } catch (err) {
+    // If it throws, return a test failure instead of crashing.
+    var message = 'This test failed because it threw an exception: "' + err + '"';
+    return __List_Cons(__Expect_fail(message), __List_Nil);
+  }
 }
