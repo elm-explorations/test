45b55633b1888a2fe084ddc87879e28881e8d2d7
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-05-26 11:19:39 -0700

    Update for Elm 0.19 beta

--- tests/test.sh
+++ tests/test.sh
@@ -8,9 +8,17 @@ rm -Rf elm_home/0.19.0/package/elm-explorations/test
 
 mkdir -p elm_home
 mkdir -p elm_home/0.19.0/package
-mkdir -p elm_home/0.19.0/package/elm-explorations/test/1.0.0
 
+mkdir -p elm_home/0.19.0/package/elm-explorations/test/1.0.0
 rsync -va ../ elm_home/0.19.0/package/elm-explorations/test/1.0.0/ --exclude tests --exclude elm-stuff --exclude .git
+
+if which runhaskell; then
+    # this produces ./versions.dat, but that file
+    # is checked in, so it's fine to skip regenerating if
+    # haskell is not available (such as on CI)
+    runhaskell MakeTestRegistry.hs
+fi
+cp ./versions.dat elm_home/0.19.0/package/versions.dat
 
 export ELM_HOME="$(pwd)"/elm_home
 
