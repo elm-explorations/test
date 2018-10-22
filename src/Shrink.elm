4001834f265d0b4d0d9098a22a8f87b5a1d1ee98
Michel Belleville <michel.belleville@gmail.com>
2018-10-22 14:39:25 +0200

    minor rephrasing

--- src/Shrink.elm
+++ src/Shrink.elm
@@ -101,8 +101,8 @@ Now let's say we shrink it to the following simpler value:
     Leaf -1
 
 It will probably take less time for the developer inspecting the failing input
-to understand what went wrong (probably when there's a `Leaf` holding a negative
-number in the tree).
+to understand what went wrong. Next, the developer could check wether a negative
+number in a leaf is what causes the test to fail.
 
 
 ### How does shrinking work?
