85e698bfbd651e02fddfbfbf086f192f36f55441
Aaron VonderHaar <av@noredink.com>
2018-11-13 20:15:45 -0800

    Fix CI build after bump to 1.2.0

--- tests/test.sh
+++ tests/test.sh
@@ -9,7 +9,7 @@ rm -Rf elm_home/0.19.0/package/elm-explorations/test
 mkdir -p elm_home
 mkdir -p elm_home/0.19.0/package
 
-PACKAGE_VERSION="1.1.0"
+PACKAGE_VERSION="1.2.0"
 mkdir -p elm_home/0.19.0/package/elm-explorations/test/${PACKAGE_VERSION}
 rsync -va ../ elm_home/0.19.0/package/elm-explorations/test/${PACKAGE_VERSION}/ --exclude tests --exclude elm-stuff --exclude .git --exclude node_modules
 
