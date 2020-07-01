7c5cce445b2077430799ccbdde61f237bc8d3a56
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-07-01 22:39:06 +0100

    use bsd sed approved inplace subsitution

--- tests/make.sh
+++ tests/make.sh
@@ -2,11 +2,19 @@
 
 set -eu
 
-sed -i 's/import Elm\.Kernel\.Debug/-- import Elm\.Kernel\.Debug/g' src/Test/Internal.elm
+# Check that this script is run from top level directory
+if [[ $(git rev-parse --show-toplevel) != $(pwd) ]]; then
+  >&2 echo "Please run ./tests/make.sh from git root directory!"
+  exit 1
+fi
+
+FILE_WITH_IMPORT=src/Test/Internal.elm
+
+sed -i.bak 's/import Elm\.Kernel\.Debug/-- import Elm\.Kernel\.Debug/g' "$FILE_WITH_IMPORT"
 
 elm make --docs=documentation.json
 exit_code=$?
 
-sed -i 's/-- import Elm\.Kernel\.Debug/import Elm\.Kernel\.Debug/g' src/Test/Internal.elm
+mv "$FILE_WITH_IMPORT.bak" "$FILE_WITH_IMPORT"
 
 exit $exit_code
