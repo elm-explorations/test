54a8051420eb13ea963e118d41d066fee2420304
Jaime Terreu <jaime.terreu@cultureamp.com>
2022-01-15 23:56:35 +1030

    feat: Expose classNS selector
    This selector will act specifically on svg classes which follow a
    different implementation to regular HTML elements.

--- src/Test/Html/Selector.elm
+++ src/Test/Html/Selector.elm
@@ -1,7 +1,7 @@
 module Test.Html.Selector exposing
     ( Selector
     , tag, text, containing, attribute, all
-    , id, class, classes, exactClassName, style, checked, selected, disabled
+    , id, class, classNS, classes, exactClassName, style, checked, selected, disabled
     )
 
 {-| Selecting HTML elements.
@@ -16,7 +16,7 @@ module Test.Html.Selector exposing
 
 ## Attributes
 
-@docs id, class, classes, exactClassName, style, checked, selected, disabled
+@docs id, class, classNS, classes, exactClassName, style, checked, selected, disabled
 
 -}
 
@@ -108,6 +108,31 @@ To match the element's exact class attribute string, use [`exactClassName`](#exa
 class : String -> Selector
 class =
     Class
+
+
+{-| Matches svg elements that have the given class (and possibly others as well).
+
+To match multiple classes at once, use [`classesNS`](#classesNS) instead.
+
+To match the element's exact class attribute string, use [`exactClassNameNS`](#exactClassNameNS).
+
+    import Svg.Html as SvgHtml
+    import Svg.Attributes as SvgAttr
+    import Test.Html.Query as Query
+    import Test exposing (test)
+    import Test.Html.Selector exposing (classNS)
+
+
+    test "Svg has the class svg-large" <|
+        \() ->
+            SvgHtml.svg [ SvgAttr.class "svg-styles svg-large" ] [  ]
+                |> Query.fromHtml
+                |> Query.has [ classNS "svg-large" ]
+
+-}
+classNS : String -> Selector
+classNS =
+    ClassNS
 
 
 {-| Matches the element's exact class attribute string.
