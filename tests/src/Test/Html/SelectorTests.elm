ed2de93479bd29dd2f61dd125ae9257329bb7ea7
Jaime Terreu <jaime.terreu@cultureamp.com>
2022-01-16 01:14:12 +1030

    feat: Add tests for all NS selectors
    I have added a few tests for attribute and class selectors to assert
    that these selectors will not work with svg classes.

--- tests/src/Test/Html/SelectorTests.elm
+++ tests/src/Test/Html/SelectorTests.elm
@@ -4,10 +4,12 @@ module Test.Html.SelectorTests exposing (all)
 -}
 
 import Fuzz exposing (..)
-import Html exposing (Html, a, div, footer, header, li, section, span, ul)
+import Html
 import Html.Attributes as Attr
+import Svg
+import Svg.Attributes as SvgAttribs
 import Test exposing (..)
-import Test.Html.Query as Query exposing (Single)
+import Test.Html.Query as Query
 import Test.Html.Selector exposing (..)
 
 
@@ -16,6 +18,104 @@ all =
     describe "Test.Html.Selector"
         [ bug13
         , textSelectors
+        , classSelectors
+        , attributeSelectors
+        , nsSelectors
+        ]
+
+
+nsSelectors : Test
+nsSelectors =
+    describe "NS selectors"
+        [ test "classNS selector finds class on svg with one class" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.has [ classNS svgClass ]
+        , test "classNS selector finds class on svg with multiple classes" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass, SvgAttribs.class "another-NS-class" ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.has [ classNS svgClass ]
+        , test "classesNS selector finds all classes on svg" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass, SvgAttribs.class "another-NS-class" ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.has [ classesNS [ svgClass, "another-NS-class" ] ]
+        , test "classesNS selector finds single class on svg with multiple classes" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass, SvgAttribs.class "another-NS-class" ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.has [ classesNS [ svgClass ] ]
+        , test "exactClassNameNS selector finds the exact class value on svg" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class another-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.has [ exactClassNameNS svgClass ]
+        ]
+
+
+attributeSelectors : Test
+attributeSelectors =
+    describe "attribute selectors"
+        [ test "attribute selector does not find class on svg elements" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.hasNot [ attribute (SvgAttribs.class svgClass) ]
+        ]
+
+
+classSelectors : Test
+classSelectors =
+    describe "class selectors"
+        [ test "does not find class on svg elements" <|
+            \() ->
+                let
+                    svgClass =
+                        "some-NS-class"
+                in
+                Svg.svg
+                    [ SvgAttribs.class svgClass ]
+                    [ Svg.circle [ SvgAttribs.cx "50", SvgAttribs.cy "50", SvgAttribs.r "40" ] [] ]
+                    |> Query.fromHtml
+                    |> Query.hasNot [ class svgClass ]
         ]
 
 
