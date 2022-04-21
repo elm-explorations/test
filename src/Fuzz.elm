e044f5cf21447a72e6365eea8f0400dcff5437ae
Martin Janiczek <martin@janiczek.cz>
2022-04-21 21:43:34 +0200

    Remove `reject`

--- src/Fuzz.elm
+++ src/Fuzz.elm
@@ -9,7 +9,7 @@ module Fuzz exposing
     , array, maybe, result
     , bool, unit, order, weightedBool
     , oneOf, oneOfValues, frequency, frequencyValues
-    , constant, reject, invalid, filter
+    , constant, invalid, filter
     , map, map2, map3, map4, map5, map6, map7, map8, andMap
     , andThen, lazy, sequence, traverse
     , fromGenerator
@@ -62,7 +62,7 @@ can usually find the simplest input that reproduces a bug.
 
 ## Working with Fuzzers
 
-@docs constant, reject, invalid, filter
+@docs constant, invalid, filter
 @docs map, map2, map3, map4, map5, map6, map7, map8, andMap
 @docs andThen, lazy, sequence, traverse
 
@@ -1300,17 +1300,6 @@ invalid reason =
                 { reason = reason
                 , prng = prng
                 }
-
-
-{-| An alias for `invalid`.
-
-A fuzzer that rejects generating values for the provided reason. Any tests using
-a `reject` fuzzer will fail.
-
--}
-reject : String -> Fuzzer a
-reject reason =
-    invalid reason
 
 
 {-| A fuzzer that only lets through values satisfying the given predicate
