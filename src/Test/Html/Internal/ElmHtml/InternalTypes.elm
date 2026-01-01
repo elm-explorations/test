76c60df43aaebfae8e101f66bad156d214b10afe
Jeroen Engels <jfm.engels@gmail.com>
2026-01-01 12:42:18 +0100

    Apply simplifications

--- src/Test/Html/Internal/ElmHtml/InternalTypes.elm
+++ src/Test/Html/Internal/ElmHtml/InternalTypes.elm
@@ -238,7 +238,7 @@ contextDecodeElmHtml context =
 decodeTextTag : Json.Decode.Decoder TextTagRecord
 decodeTextTag =
     field kernelConstants.virtualDom.text
-        (Json.Decode.andThen (\text -> Json.Decode.succeed { text = text }) Json.Decode.string)
+        (Json.Decode.map (\text -> { text = text }) Json.Decode.string)
 
 
 {-| decode a tagger
