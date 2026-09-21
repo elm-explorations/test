2838d31f401c73d9b590b5623570871814c11cca
Martin Janiczek <martin@janiczek.cz>
2026-09-21 21:24:39 +0200

    Fix `repeat` docs' order of arguments to `fuzz`

--- src/Test.elm
+++ src/Test.elm
@@ -596,7 +596,7 @@ can use `repeat` to save such inputs as explicit regression tests:
 
     repeat "String.words never returns an empty list"
         [ testWithValue "only whitespace" " \n\t"
-        , fuzz Fuzz.string "fuzz"
+        , fuzz "fuzz" Fuzz.string
         ]
     <|
         \string ->
@@ -611,7 +611,7 @@ function composition (`<<`):
 
     repeat "String.words never returns an empty list"
         [ only << testWithValue "only whitespace" " \n\t"
-        , fuzz Fuzz.string "fuzz"
+        , fuzz "fuzz" Fuzz.string
         ]
     <|
         \string ->
